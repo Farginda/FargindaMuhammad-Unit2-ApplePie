5eff730cd69817def6ddd33264d56b7388c4dd50
Farginda <Farginda@luuks-macbook-pro.local>
2018-11-09 13:08:17 +0100

    Eind project Apple Pie

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,12 +9,87 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // list of words for the game
+    var listOfWords = ["swift", "program", "technologie", "food", "school", "fitness", "love", "movies", "beautiful", "instagram"]
+    
+    // users are allowed to make 7 incorrect moves
+    let incorrectMovesAllowed = 7
+    
+    // start new round after each win or loss
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    // actions if user presses button
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    
+    // updates the game state
+    func updateGameState () {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        newRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    var currentGame: Game!
+    
+    // tapped button can't be pressed again during same game
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
 
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord.characters {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+    // function for start new round
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+
+    }
+    
 }
-
